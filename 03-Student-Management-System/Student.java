dd9656b79d25365e10211ca6661a1bbff25e0298
mgounaridi <mgounaridi@icloud.com>
2026-09-09 15:06:34 +0200

    Student class

--- 03-Student-Management-System/Student.java
+++ 03-Student-Management-System/Student.java
@@ -1,98 +1,83 @@
-package LisisThematon;
-
-/**
- * Λύση τέταρτου θέματος πρώτο μέρος
- *  Δημιουργία κλάσης Student
- * @author MariaIouliaGounaridi
- */
 public class Student {
-    //Η κλάση με όνομα Student, που θα δημιουργήσω τον σχεδιασμό του αντικειμένου
-    //Παρακάτω εισάγω τις ιδιότητες του φοιτητή
-    private String onoma;// Μεταβλητή που αποθηκεύει το όνομα του φοιτητή
-    private double vathmos;//Μεταβλητή που αποθηκεύει τον βαθμό του φοιτητή
-    private String idikotita;//Μεταβλητή που αποθηκεύει την ειδικότητα του φοιτητή
-    private int etosSpoudon;//Μεταβλητή που αποθηκεύει το έτος σπουδών του φοιτητή
     
-    //Δημιουργώ τον καταστευαστή που θα καλείται κάθε φορά που δημιουργώ ένα αντικείμενο Student
+    private String onoma;
+    private double vathmos;
+    private String idikotita;
+    private int etosSpoudon;
+    
     /**
      * 
-     * @param onoma (Το όνομα του φοιτητή)
-     * @param vathmos (Ο βαθμός του φοιτητή) 
-     * @param idikotita (Η ειδικότητά του φοιτητή)
-     * @param etosSpoudon (Το έτος σπουδών του φοιτητή)
+     * @param onoma (Name of Student)
+     * @param vathmos (Grade of Student) 
+     * @param idikotita (Major of Student)
+     * @param etosSpoudon (Year of Studies)
      */
 public Student(String onoma,double vathmos,String idikotita,int etosSpoudon) {
-    //Χρησιμοποιώ το this ωστε να ξεχωρίσω τις ιδιότητες της κλάσης απο τις παραμέτρους
-    this.onoma=onoma;//Ώστε να αποθηκεύει το όνομα του φοιτητή
-    this.vathmos=vathmos;//Ώστε να αποθηκεύει τον βαθμό του φοιτητή 
-    this.idikotita=idikotita;//Ώστε να αποθηκεύει την ειδικότητα του φοιτητή
-    this.etosSpoudon=etosSpoudon;//Ώστε να αποθηκεύει το έτος σπουδών του φοιτητή
+    
+    this.onoma=onoma;
+    this.vathmos=vathmos;
+    this.idikotita=idikotita;
+    this.etosSpoudon=etosSpoudon;
     
 }
-
-//Δημιουργώ Setters και Getters, ώστε να μπορω να εισάγω και να διαβάζω τις μεταβλητές στην main
    
     /**
      * @return όνομα
-     * Αυτή η μέθοδος επιστρέφει το όνομα του φοιτητή
+     * Returns the name of the Student
      */
     public String getOnoma() {
         return onoma;
     }
     /**
      * @param onoma 
-     * Αυτή η μέθοδος επιτρέπει να ορίσουμε το όνομα του φοιτητή στη main
      */
     public void setOnoma(String onoma) {
         this.onoma=onoma;
     }
     /**
      * @return βαθμός
-     * Αυτή η μέθοδος επιστρέφει τον βαθμό του φοιτητή 
+     * Returns the Grade of the Student
      */
     public double getVathmos() {
         return vathmos;
     }
     /**
      * @param vathmos 
-     * Αυτή η μέθοδος μας επιτρέπει να ορίσουμε τον βαθμό του φοιτητή στη main
      */
     public void setVathmos(double vathmos) {
         this.vathmos=vathmos;
     }
     /**
      * @return ειδικότητα
-     * Αυτή η μέθοδος επιστρέφει την ειδικότητα του φοιτητή
+     * Returns the major of the student
      */
     public String getIdikotita() {
         return idikotita;
     }
     /**
      * @param idikotita 
-     * Αυτή η μέθοδος μας επιτρέπει να ορίσουμε την ειδικότητα του φοιτητή στη main 
      */
     public void setIdikotita(String idikotita) {
         this.idikotita=idikotita;
     }
     /**
      * @return έτος σπουδών
-     * Αυτή η μέθοδος επιστρέφει το έτος σπουδών
+     * Returns the year of Studies
      */
     public int getEtosSpoudon() {
         return etosSpoudon;
     }
     /**
      * @param etosSpoudon 
-     * Αυτή η μέθοδος μας επιτρέπει να ορίσουμε το έτος των σπουδών του φοιτητή στη main
      */
     public void setEtosSpoudon(int etosSpoudon) {
         this.etosSpoudon=etosSpoudon;
     }
     
     /**
-     * @return true πέρασe διαφορετικά, fasle αν κόπηκε
-     * Αυτή η μέθοδος, ελέγχει αν ο φοιτητής έχει περάσει το μάθημα 
-     * Αν ο βαθμός ειναι μεγαλύτερος ή ίσος του 40 έχει περάσει 
+     * @return true 
+     * Checks if a student passed 
+     * With grade greater than or equal to 40
      */
     public boolean isPassed() {
         if (this.vathmos>=40) {
@@ -102,4 +87,4 @@ public Student(String onoma,double vathmos,String idikotita,int etosSpoudon) {
         }
     }
    
-} //Τέλος Κλάσης
+}
